26ec3159482ad9a95b04b91216c6a11d53f8de22
lostedzahfia <oyekunlefarouk2001@gmail.com>
2022-01-07 12:58:29 +0100

    added files

---
+++ number1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace number1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+        static void Plist(List<int> k, int i)
+        {
+            for(int p = i; p < k.Count; p++)
+            {
+                Console.Write(k[p] + " ");
+                Console.WriteLine();
+            }
+        }
+      
+
+    }
+}
