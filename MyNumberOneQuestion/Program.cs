80846155234f52c6bf89c9063d4b549c0acb86d4
lostedzahfia <oyekunlefarouk2001@gmail.com>
2022-01-14 12:14:04 +0100

    january 14th assignment

---
+++ MyNumberOneQuestion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyNumberOneQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            List<int> Input = new List<int>(){7 , 10, 18, 06, 23, 27, 29, 66,26, 25};
+            LargestInPut(Input);
+           // Console.WriteLine();
+        }
+        public static void LargestInPut(List<int> Input)
+        {
+            string ReturnOutput = string.Empty;
+            List<string> newInputList = Input.ConvertAll<string>
+            (delegate(int x){return x.ToString();});
+            newInputList.Sort(CompareElement);
+
+            for(int k = 0; k < Input.Count; k++)
+            {
+                ReturnOutput = ReturnOutput + newInputList[k];
+            }
+            if(ReturnOutput[0] == '0' && ReturnOutput.Length > 1)
+            {
+                Console.Write("0");
+            }
+            Console.Write(ReturnOutput);
+        }
+        public static int CompareElement(string i, string j)
+        {
+            string ij = i + j;
+            string ji = j + i;
+            
+            return ij.CompareTo(ji) > 0 ? - 1 : 1;
+
+        }
+    }
+}
---
+++ MyNumberTwoQuestion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyNumberTwoQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int []arr = {1, 3, 2, 5, 7, 3 };
+           int k = arr.Length;
+
+           k = DuplicateValue(arr, k);
+
+           for(int i = 0; i < k; i++)
+           {
+               Console.WriteLine(arr[i] );
+           }
+        }
+        
+    }
+}
